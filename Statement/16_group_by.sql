cdca21015fa756e84c0fadaa2a83d8407e3129e8
117Castro <131223103+117Castro@users.noreply.github.com>
2025-03-18 16:55:16 -0500

    Update 16_group_by.sql

--- Statement/16_group_by.sql
+++ Statement/16_group_by.sql
@@ -1,15 +1,15 @@
-SELECT MAX(age) AS 'minima edad' FROM users GROUP BY surname;
+SELECT MAX(age) AS 'maxima edad' FROM users GROUP BY surname;
 
 SELECT MIN(age) AS 'minima edad' FROM users GROUP BY surname;
 
-SELECT MAX(age) AS 'minima edad' FROM users GROUP BY age;
+SELECT MAX(age) AS 'maxima edad' FROM users GROUP BY age;
 
-SELECT MAX(age) AS 'minima edad', age FROM users WHERE age > 30 AND age < 50 GROUP BY age ORDER BY MAX(age) ASC ;
+SELECT MAX(age) AS 'maxima edad', age FROM users WHERE age > 30 AND age < 50 GROUP BY age ORDER BY MAX(age) ASC ;
 --si queremos agregar condiciones nesesitamos colocar la sentencia WHERE como restriccion, 
 --si no lo colocamos el GROUP BY que es de filtrado tomara los parametros como booleanos. 
 --SELECT MAX(age) AS 'minima edad', age FROM users GROUP BY age > 30 AND age < 50 ORDER BY MAX(age) ASC ;
 
-SELECT MAX(age) AS 'minima edad', age FROM users GROUP BY age ;
+SELECT MAX(age) AS 'maxima edad', age FROM users GROUP BY age ;
 
 /*La GROUP BY declaración agrupa las filas que tienen los mismos valores 
 en filas de resumen, como "encontrar el número de clientes en cada país".
